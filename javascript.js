209f1020f04b409c61ade481237257b0f7bbcca4
Alex Bennett <kestralttr@gmail.com>
2017-08-28 21:24:59 -0700

    makes test requests to IMDB api

--- javascript.js
+++ javascript.js
@@ -1,16 +1,54 @@
 
 (function() {
+  let baseUrl = "http://www.theimdbapi.org/api/";
+  let method = "GET";
 
-  function makeRequest() {
-    console.log("request is made!");
+  let requestTypes = {
+    movieTitle:"find/movie?title=",
+    person:"find/person?name=",
+    movieID:"movie?movie_id=",
+    actorID:"person?person_id="
+  };
+
+  let activeRequestType = requestTypes.movieTitle;
+
+  function getSearchParam(inputField) {
+    // inputField must be a jQuery element
+    return inputField.val();
+  }
+
+  function makeRequest(inputField) {
+
+    let searchParam = getSearchParam(inputField);
+    console.log("searchParam: ", searchParam);
+    if(searchParam === "") {
+      console.log("SEARCHPARAM IS EMPTY!");
+      return;
+    }
+
+    let finalUrl = baseUrl + activeRequestType + searchParam;
+    console.log("finalUrl: ", finalUrl);
+
+    $.ajax({
+      url: finalUrl,
+      method: method
+    })
+    .done(function(data) {
+      console.log("IMBD data returned");
+      console.log(data);
+    })
+    .fail(function(xhr) {
+      return xhr;
+    });
   }
 
   $("document").ready(function() {
     let submitButton = $(".submit-button");
+    let inputField = $(".input-field");
 
     submitButton.click(function(e) {
       e.preventDefault();
-      makeRequest();
+      makeRequest(inputField);
     });
 
 
